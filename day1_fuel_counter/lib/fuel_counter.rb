cbab6146eb8ef419c24e851524a342fa42420917
molbrown <molly@spreedly.com>
2019-12-01 19:14:05 -0500

    Part 2 - updates fuel_calculate with while loop to accommodate for fuel requirements, also updates tests

--- day1_fuel_counter/lib/fuel_counter.rb
+++ day1_fuel_counter/lib/fuel_counter.rb
@@ -1,15 +1,20 @@
 class FuelCounter
   def initialize(input)
-		@modules = input
-	end
-	
-	def fuel_total
-		@modules.inject(0) {|sum, mass| sum + fuel_calculate(mass.to_f) }
-	end
+    @modules = input
+  end
 
-	def fuel_calculate(mass)
-		(mass/3).to_i - 2
-	end
+  def fuel_total
+    @modules.inject(0) { |sum, mass| sum + fuel_calculate(mass.to_f) }
+  end
+
+  def fuel_calculate(mass)
+    total = 0
+    while mass > 0
+      mass = (mass/3).to_i - 2
+      total += mass if mass > 0
+    end
+    total
+  end
 end
 
 puts FuelCounter.new(File.readlines('input')).fuel_total
--- day1_fuel_counter/test/fuel_counter_test.rb
+++ day1_fuel_counter/test/fuel_counter_test.rb
@@ -5,12 +5,12 @@ class FuelCounterTest < Test::Unit::TestCase
   def test_fuel_calculate
     assert_equal(2, check_calculate(12))
     assert_equal(2, check_calculate(14))
-    assert_equal(654, check_calculate(1969))
-    assert_equal(33583, check_calculate(100756))
+    assert_equal(966, check_calculate(1969))
+    assert_equal(50346, check_calculate(100756))
   end
 
   def test_total
-    assert_equal(34241, check_total([12, 14, 1969, 100756]))
+    assert_equal(51316, check_total([12, 14, 1969, 100756]))
   end
 
   private
